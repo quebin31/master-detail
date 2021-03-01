cd6d97c023e0efa2690294308ad75ba6f2c6cb9e
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-03-01 13:32:50 -0500

    Setup basic skeleton for fragments.

---
+++ app/src/main/java/co/ravn/kevin/masterdetail/ui/PostsFragment.kt
@@ -0,0 +1,33 @@
+package co.ravn.kevin.masterdetail.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.ravn.kevin.masterdetail.databinding.FragmentPostsBinding
+import co.ravn.kevin.masterdetail.networking.Api
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class PostsFragment : Fragment() {
+
+    private var _binding: FragmentPostsBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject lateinit var api: Api
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPostsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
