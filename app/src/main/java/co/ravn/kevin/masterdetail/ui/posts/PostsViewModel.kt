0b5d404e4a3b9b5791b9c551c415319f2c3cd938
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-03-01 17:01:04 -0500

    Fix view model not being tagged with hilt annotation.

--- app/src/main/java/co/ravn/kevin/masterdetail/ui/posts/PostsViewModel.kt
+++ app/src/main/java/co/ravn/kevin/masterdetail/ui/posts/PostsViewModel.kt
@@ -1,16 +1,14 @@
 package co.ravn.kevin.masterdetail.ui.posts
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import co.ravn.kevin.masterdetail.model.Post
 import co.ravn.kevin.masterdetail.model.Result
 import co.ravn.kevin.masterdetail.networking.Api
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class PostsViewModel @Inject constructor(private val api: Api) : ViewModel() {
-    suspend fun getPosts(): Result<List<Post>> {
-        return api.getPosts()
-    }
+
+    suspend fun getPosts(): Result<List<Post>> = api.getPosts()
 }
