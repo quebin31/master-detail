87cbff6823b167d282aac5a42a226b7f04288c59
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-03-01 14:22:05 -0500

    Add dagger module for networking.

---
+++ app/src/main/java/co/ravn/kevin/masterdetail/hilt/NetworkingModule.kt
@@ -0,0 +1,39 @@
+package co.ravn.kevin.masterdetail.hilt
+
+import co.ravn.kevin.masterdetail.networking.Api
+import co.ravn.kevin.masterdetail.networking.ApiService
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkingModule {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
+
+    private fun buildClient(): OkHttpClient = OkHttpClient.Builder().run {
+        build()
+    }
+
+    private fun buildRetrofit(): Retrofit = Retrofit.Builder().run {
+        val gson = GsonBuilder().setLenient().create()
+
+        client(buildClient())
+        baseUrl(BASE_URL)
+        addConverterFactory(GsonConverterFactory.create(gson))
+        build()
+    }
+
+    private fun buildApiService(): ApiService = buildRetrofit().create(ApiService::class.java)
+
+    @Provides
+    fun provideRetrofitApi(): Api {
+        val apiService = buildApiService()
+        return Api(apiService)
+    }
+}
