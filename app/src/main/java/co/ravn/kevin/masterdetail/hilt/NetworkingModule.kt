f740ac6f82d193e75a3a5b425de0ab5d354d44cf
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-03-01 17:05:19 -0500

    Add logging interceptor for http client.

--- app/src/main/java/co/ravn/kevin/masterdetail/hilt/NetworkingModule.kt
+++ app/src/main/java/co/ravn/kevin/masterdetail/hilt/NetworkingModule.kt
@@ -8,6 +8,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,6 +18,9 @@ object NetworkingModule {
     private const val BASE_URL = "https://jsonplaceholder.typicode.com"
 
     private fun buildClient(): OkHttpClient = OkHttpClient.Builder().run {
+        addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
         build()
     }
 
